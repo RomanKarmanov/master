a4fb51013d9a72b16180759e03635de0a92f8b26
Roman Karmanov <way_g@mail.ru>
2018-10-07 17:39:38 +0300

    video_and_btn_menu

--- src/assets/scripts/about.js
+++ src/assets/scripts/about.js
@@ -1,2 +1,3 @@
 import "./modules/skills";
 import "./modules/map";
+import "./modules/burger"
---
+++ src/assets/scripts/modules/burger.js
@@ -0,0 +1,6 @@
+let burger = document.querySelector("#burger");
+let fullScreen = document.querySelector("#full-screen");
+
+burger.addEventListener("click", function(){
+    fullScreen.style = 'displey:flex';
+});
